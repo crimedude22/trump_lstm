e134d374322ece6072ab6a954b44fd4e873ac09c
crimedude22 <JLSaunders987@gmail.com>
2017-01-25 12:54:55 -0800

    Simple character-level LSTM to model trump's speech

---
+++ trump_lstm.py
@@ -0,0 +1,121 @@
+from __future__ import print_function
+import nltk
+import codecs
+import re
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.optimizers import RMSprop, Adam
+from keras.regularizers import l2
+
+import numpy as np
+import random
+import sys
+
+maxlen = 60
+step = 5
+
+# Directly tokenize
+#speech_tokens = nltk.word_tokenize(speech_file.read())
+#len(set(speech_tokens))
+
+def prep_strings(maxlen,step):
+    speech_file = codecs.open('/trumpdox/all_combined_speeches.txt','r','utf-8-sig')
+
+    # Get as lines first to shuffle
+    speech_raw = [line for line in speech_file]
+    random.shuffle(speech_raw)
+
+    # Join lines, tokenize & join, then eliminate whitespace before punctuation
+    speech_raw = ' '.join(speech_raw)
+    speech_raw = ' '.join(nltk.word_tokenize(speech_raw))
+    speech_raw = re.sub("(?<=\w) (?=\.)","",speech_raw) # def
+    speech_raw = re.sub("(?<=\w) (?=\?)","",speech_raw) # more
+    speech_raw = re.sub("(?<=\w) (?=')","",speech_raw)  # better
+    speech_raw = re.sub("(?<=\w) (?=,)","",speech_raw)  # wayz
+
+    #following https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
+    # Make dicts of chars
+    chars = sorted(list(set(speech_raw)))
+    char_indices = dict((c, i) for i, c in enumerate(chars))
+    indices_char = dict((i, c) for i, c in enumerate(chars))
+
+    # cut the text in semi-redundant sequences of maxlen characters
+    sentences = []
+    next_chars = []
+    for i in range(0, len(speech_raw) - maxlen, step):
+        sentences.append(speech_raw[i: i + maxlen])
+        next_chars.append(speech_raw[i + maxlen])
+    print('nb sequences:', len(sentences))
+
+    # Vectorize sentences
+    X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+    y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+    for i, sentence in enumerate(sentences):
+        for t, char in enumerate(sentence):
+            X[i, t, char_indices[char]] = 1
+        y[i, char_indices[next_chars[i]]] = 1
+
+    return X,y
+
+# Build keras model
+model = Sequential()
+# Standard LSTM w/ dropout
+model.add(LSTM(128, input_shape=(maxlen, len(chars)),
+          dropout_W=0.05,dropout_U=0.1,
+          W_regularizer=l2(0.01),U_regularizer=l2(0.01),
+          ))
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+
+optimizer = Adam(lr=0.001) # RMSProp w/ momentum
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+# train the model, output generated text after each iteration
+for iteration in range(1, 10):
+    print()
+    print('-' * 50)
+    print('Iteration', iteration)
+    print('Generating Strings')
+    X,y = prep_strings(maxlen,step)
+
+    model.fit(X, y, batch_size=256, nb_epoch=1)
+
+    start_index = random.randint(0, len(speech_raw) - maxlen - 1)
+
+    # Generate test strings
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        print()
+        print('----- diversity:', diversity)
+
+        generated = ''
+        sentence = speech_raw[start_index: start_index + maxlen]
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+
+        for i in range(400):
+            x = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x[0, t, char_indices[char]] = 1.
+
+            preds = model.predict(x, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
+
+            generated += next_char
+            sentence = sentence[1:] + next_char
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
